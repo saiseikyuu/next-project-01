09a4e41b9034f27976d9e7109af1593534dc9268
saiseikyuu <saiseikyuu@gmail.com>
2025-06-30 23:11:24 +0800

    Update

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="min-h-screen bg-white text-black  w-full  ">
+      <div className=" w-full shadow-2xl rounded-lg px-5 py-8 text-2xl  max-w-2xl mx-auto flex space-y-8">
+        <h1 className="font-extrabold">Dashboard</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+import Link from "next/link";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="flex flex-col min-h-screen bg-white 100 items-center justify-center p-6">
+        <div className="text-center">
+          <h1 className=" text-5xl font-bold  mb-6">
+            Welcome to Dashboard App
+          </h1>
+          <p className="text-gray-600 mb-8">
+            Manage your tasks, track analytics, and stay organized with our
+            powerful dashboard
+          </p>
+          <Link
+            href={"/dashboard"}
+            className=" border px-8 py-3 rounded-xl active:bg-white active:text-black font-bold"
+          >
+            Go To Dashboard
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
